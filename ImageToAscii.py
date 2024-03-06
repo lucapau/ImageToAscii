bac32c37929200c9f701647eb182a505436e1719
“Luca <2621045p@student.gla.ac.uk>
2024-03-06 14:27:38 +0000

    resizing and grayscalling

--- ImageToAscii.py
+++ ImageToAscii.py
@@ -1,5 +1,16 @@
-#Loading the image in
 
 from PIL import Image
+
+#Loading the image in
+
 im = Image.open(r'C:\Users\Luca\Pictures\cat.webp')
 print(im.format, im.size, im.mode)
+
+# resizing and grayscalling
+
+newsize = (1000, 1000)
+im = im.resize(newsize)
+# grayscale
+im = im.convert('L') 
+im.show()
+
