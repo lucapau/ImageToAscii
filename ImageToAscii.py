5c33a2f2cfe90d319512b99cbb7a41a661c3fae6
“Luca <2621045p@student.gla.ac.uk>
2024-03-06 14:59:19 +0000

    ascii convertion from grayscale pixel values

--- ImageToAscii.py
+++ ImageToAscii.py
@@ -3,14 +3,26 @@ from PIL import Image
 
 #Loading the image in
 
-im = Image.open(r'C:\Users\Luca\Pictures\cat.webp')
+im = Image.open(r'C:\Users\Luca\Pictures\monalisa.jpg')
 print(im.format, im.size, im.mode)
 
 # resizing and grayscalling
 
-newsize = (1000, 1000)
+newsize = (100, 100)
 im = im.resize(newsize)
 # grayscale
 im = im.convert('L') 
-im.show()
+WIDTH, HEIGHT = im.size
 
+
+#assigning ascii characters to graysclae pixel values
+
+data = list(im.getdata()) 
+
+data = [data[offset:offset+WIDTH] for offset in range(0, WIDTH*HEIGHT, WIDTH)]
+
+ascii_chars = "@$%#*+=-:."
+scale = (len(ascii_chars)-1)/255.
+print()
+for row in data:
+    print(''.join(ascii_chars[int(value*scale)] for value in row))
